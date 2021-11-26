2e97116d9b9cd2b1e3ba54cf847814f4d4da2039
Bernardo Manciola Lobo <62481787+tlamen@users.noreply.github.com>
2021-11-26 11:22:41 -0300

    Adicionei validação pro preço ser maior que 0

--- SiriCascudo/app/models/meal.rb
+++ SiriCascudo/app/models/meal.rb
@@ -4,6 +4,8 @@ class Meal < ApplicationRecord
   
   validates_uniqueness_of :name
   validates :name, :description, :price, presence: true
+  
+  validates :price, numericality: { greater_than: 0 }
 
   has_one_attached :picture
 end
