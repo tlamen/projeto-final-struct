c63f20774cfc6f136b76bad810bfd7e03707ef32
artistrea <apiravesi@gmail.com>
2021-12-07 13:49:32 -0300

    Add imagens e hotfix em serializer meals

--- SiriCascudo/db/seeds.rb
+++ SiriCascudo/db/seeds.rb
@@ -7,23 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 categories = Category.create([{name: 'hamburgueres'}, {name: 'acompanhamentos'}, {name: 'bebidas'}, {name: 'sobremesas'}])
-meals = Meal.create([{name: 'Hamburguer de siri', category_id: 1, price: 17.00, description: "Pão, 180g de carne de siri, queijo, alga e tomate"},
+meals = Meal.create([
+    {name: 'Hambúrguer de siri', category_id: 1, price: 17.00, description: "Pão, 180g de carne de siri, queijo, alga e tomate"},
     {name:"Hambúrguer de Siri Monstruoso", category_id: 1, price: 28.00, description: "Pão, 2x180g de carne de siri, queijo, ovas fritas, alga e tomate"},
     {name:"Hambúrguer de Siri com Geléia", category_id: 1, price: 20.50, description: "Pão, 180g de carne de siri, queijo, alga, tomate e geléia de água-viva monstruosa"},
     {name:"Mini hambúrguer de siri", category_id: 1, price: 13.00, description: "Pão, 90g de carne de siri, queijo, alga e tomate"},
     {name:"Siri supremo triplo", category_id: 1, price: 35.50, description: "Pão, 3x180g de carne de siri, queijo, ovas fritas, alga e tomate"},
-    {name:"Hambúrguer de Siri de Luxo", category_id: 1, price: 90.00, description: "Pão australiano, 180g de carne de siri, caviar e limão"},
+    {name:"Hambúrguer de Siri de Luxo", category_id: 1, price: 90.00, description: "Pão australiano, 270g de carne de siri, caviar, limão, alga e tomate"},
 
-    {name: 'Batata frita pequena', category_id: 2, price: 7.50, description: "50 batatas fritas"},
-    {name: 'Batata frita média', category_id: 2, price: 9.00, description: "75 batatas fritas"},
-    {name: 'Batata frita grande', category_id: 2, price: 10.50, description: "100 batatas fritas"},
+    {name: 'Batata frita', category_id: 2, price: 7.50, description: "50 batatas fritas"},
+    {name: 'Pedaços de coral', category_id: 2, price: 7.50, description: "25 pedaços de coral"},
+    {name: 'Anéis de algas', category_id: 2, price: 7.50, description: "25 anéis de algas"},
 
     {name: 'Água sem sal', category_id: 3, price: 6.50, description: "Jarra de 500ml de água sem sal"},
     {name: 'Refrigerante em lata', category_id: 3, price: 6.50, description: "Lata de 310ml de: coca-cola (normal ou zero) e fanta (laranja, uva ou guaraná) "},
 
-    {name: 'Sirishake pequeno', category_id: 4, price: 12.50, description: "Milkshake 300ml de: morango, chocolate, frutas vermelhas ou baunilha"},
-    {name: 'Sirishake médio', category_id: 4, price: 15.50, description: "Milkshake 400ml de: morango, chocolate, frutas vermelhas ou baunilha"},
-    {name: 'Sirishake grande', category_id: 4, price: 18.50, description: "Milkshake 550ml de: morango, chocolate, frutas vermelhas ou baunilha"}
+    {name: 'Sirishake de morango', category_id: 4, price: 15.50, description: "Milkshake 400ml de chocolate"},
+    {name: 'Sirishake de chocolate', category_id: 4, price: 15.50, description: "Milkshake 400ml de morango"}
     ])
+meals.each do |meal| 
+    meal.picture.attach(io: File.open("./imgs/meal/#{(meal.name)}.jpg"), filename: "#{(meal.name)}.jpg")
+end
 user = User.create({email: "jao@jao" ,name: "jao" , password: "jaojao"  })
 admin = User.create({email: "adm@adm", name: "adeministrador", password: "123456", is_admin: true})
